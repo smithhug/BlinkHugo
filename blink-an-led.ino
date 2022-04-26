a88780ea7a08201bd035fe66a78689a4de2cfdef
smithhug <102279262+smithhug@users.noreply.github.com>
2022-04-26 13:32:06 +1000

    Add files via upload

---
+++ blink-an-led.ino
@@ -0,0 +1,84 @@
+int led2 = D7; // Instead of writing D7 over and over again, we'll write led2
+void setup() {
+  pinMode(led2, OUTPUT);
+
+}
+
+void loop() {
+
+  // H
+  digitalWrite(led2, HIGH);
+  delay(500);
+  digitalWrite(led2, LOW);
+  delay(500);
+    // .
+  digitalWrite(led2, HIGH);
+  delay(500);
+  digitalWrite(led2, LOW);
+  delay(500);
+    // .
+  digitalWrite(led2, HIGH);
+  delay(500);
+  digitalWrite(led2, LOW);
+  delay(500);
+    // .
+  digitalWrite(led2, HIGH);
+  delay(500);
+  digitalWrite(led2, LOW);
+  
+  delay(2000);
+
+  // U
+  digitalWrite(led2, HIGH);
+  delay(500);
+  digitalWrite(led2, LOW);
+  delay(500);
+  // .
+  digitalWrite(led2, HIGH);
+  delay(500);
+  digitalWrite(led2, LOW);
+  delay(500);
+  // -
+  digitalWrite(led2, HIGH);
+  delay(1000);
+  digitalWrite(led2, LOW);
+  
+  delay(2000);
+  
+    // G
+  digitalWrite(led2, HIGH);
+  delay(1000);
+  digitalWrite(led2, LOW);
+  delay(500);
+    // -
+  digitalWrite(led2, HIGH);
+  delay(1000);
+  digitalWrite(led2, LOW);
+  delay(500);
+      // .
+  digitalWrite(led2, HIGH);
+  delay(500);
+  digitalWrite(led2, LOW);
+  
+  delay(2000);
+  
+    // O
+  digitalWrite(led2, HIGH);
+  delay(1000);
+  digitalWrite(led2, LOW);
+  delay(500);
+    // -
+  digitalWrite(led2, HIGH);
+  delay(1000);
+  digitalWrite(led2, LOW);
+  delay(500);
+    // -
+  digitalWrite(led2, HIGH);
+  delay(1000);
+  digitalWrite(led2, LOW);
+  
+  
+  
+  delay(2000);
+}
+
